c4e4af00d4d893c6877a1d31f85a7a68d6306b4e
Abir Rakshit <abir.raksh@gmail.com>
2025-07-19 14:16:21 +0600

    added custom animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 // components/Hero.jsx
 import { motion } from "framer-motion";
 import bgImage from "../assets/avatar.jpg";
+import "../styles/custom_1.css";
 
 const Hero = () => {
 	return (
@@ -8,25 +9,27 @@ const Hero = () => {
 			className="relative h-screen w-full bg-cover bg-center flex items-center justify-center text-white"
 			style={{ backgroundImage: `url(${bgImage})` }}
 		>
-			<div className="flex justify-center items-center backdrop-blur-xl h-full w-full m-7 rounded-2xl border-e-2 border-white/30">
-				<div className="bg-black/60 p-8 rounded-xl shadow-lg text-center">
-					<motion.h1
-						initial={{ opacity: 0, y: -50 }}
-						animate={{ opacity: 1, y: 0 }}
-						transition={{ duration: 1 }}
-						className="text-4xl md:text-6xl font-bold font-mono"
-					>
-						Abir Rakshit
-					</motion.h1>
-					<motion.p
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						transition={{ delay: 0.5, duration: 1 }}
-						className="mt-4 text-lg md:text-xl italic"
-					>
-						Samurai Web Developer ⚔️ | Hacker in Training 💻 | Tech-King Dreamer
-						👑
-					</motion.p>
+			<div className="customShape bg-sky-300/20 backdrop-blur-md h-full w-full">
+				<div className="flex justify-center items-center h-full w-full m-7 rounded-2xl border-e-2 border-white/30">
+					<div className="bg-black/60 p-8 rounded-xl shadow-lg text-center">
+						<motion.h1
+							initial={{ opacity: 0, y: -50 }}
+							animate={{ opacity: 1, y: 0 }}
+							transition={{ duration: 1 }}
+							className="text-4xl md:text-6xl font-bold font-mono"
+						>
+							Abir Rakshit
+						</motion.h1>
+						<motion.p
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							transition={{ delay: 0.5, duration: 1 }}
+							className="mt-4 text-lg md:text-xl italic"
+						>
+							Samurai Web Developer ⚔️ | Hacker in Training 💻 | Tech-King
+							Dreamer 👑
+						</motion.p>
+					</div>
 				</div>
 			</div>
 		</div>
